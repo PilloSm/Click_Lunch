b41cfa81880fd00c3f9d1f925c751a3563d29740
nietomar <124025713+nietomar@users.noreply.github.com>
2023-11-26 21:19:01 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,5 @@ import FormInicio from "@/components/formInicio";
 import { signOut } from "next-auth/react";
 
 export default function Home() {
-  return <><FormInicio />
-  <button className="flex bg-blue-200 h-10 w-7" onClick={()=>{signOut()}}>Cerrar Session</button>
-  </>;
+  return <FormInicio />
 }
